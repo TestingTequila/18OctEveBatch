1a72554de29be2642c9cfc4acf8da6d1d798127d
Ashish Mishra <ashish.mishra.sse@gmail.com>
2022-11-07 17:26:50 +0530

    First Commit   Comments   Print

---
+++ src/javabasics/_1LearningComments.java
@@ -0,0 +1,25 @@
+package javabasics;
+
+public class _1LearningComments {
+
+	public static void main(String[] args) {
+
+		// 1. Single Line Comment
+		
+		/* 
+		   2. Multiline Comments 
+		 */
+		
+		
+	    
+	    
+	    
+	    
+	    
+		
+           
+
+	}
+
+}
+
---
+++ src/javabasics/_2LearningPrint.java
@@ -0,0 +1,17 @@
+package javabasics;
+
+public class _2LearningPrint {
+
+	public static void main(String[] args) {
+
+        // Print anything in Java [sysout+Control+SB]
+
+		System.out.println("Today is Friday");
+
+		System.out.println("We are learning Java");
+
+		System.out.println("My name is Ashish");
+
+	}
+
+}
